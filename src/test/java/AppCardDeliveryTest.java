a9a6b420a9c74bace11e330ce3b3284a4e42a38d
Валентина Юрова <visible7@mail.ru>
2022-01-16 17:39:42 +0500

    добавлены автотесты проверки отправки формы

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,13 +1,17 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.Assert.*;
 
 public class AppCardDeliveryTest {
     @Test
@@ -18,7 +22,10 @@ public class AppCardDeliveryTest {
         Date futureDate = DateUtils.addDays(dateNow, 5);
         SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
         String dateForInput = formatForDateNow.format(futureDate);
-        $("[data-test-id='date'] input").setValue(dateForInput);
+        SelenideElement dateField = $("[data-test-id='date'] input");
+        dateField.sendKeys(Keys.CONTROL + "a");
+        dateField.sendKeys(Keys.DELETE);
+        dateField.setValue(dateForInput);
 
         $("[data-test-id='name'] input").setValue("Петров Игорь");
         $("[data-test-id='phone'] input").setValue("+79226584321");
@@ -28,10 +35,63 @@ public class AppCardDeliveryTest {
                 .click();
         $("fieldset").shouldHave(Condition.attribute("disabled"), Duration.ofSeconds(2));
         $("fieldset").shouldNotHave(Condition.attribute("disabled"), Duration.ofSeconds(15));
+        $("[data-test-id='notification']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(5))
+                .shouldHave(Condition.text("Успешно!\nВстреча успешно забронирована на " + dateForInput));
+
     }
 
     @BeforeEach
     public void openBrowser() {
         open("http://localhost:9999");
+    }
+
+    @Test
+    public void shouldCreateOrderWithExtendedControls() {
+
+        SelenideElement cityField = $("[data-test-id='city'] input");
+        cityField.sendKeys("С");
+        cityField.sendKeys("а");
+        $(".input__popup").shouldBe(Condition.visible, Duration.ofSeconds(5));
+        ElementsCollection cityMenuItems = $$(".menu-item");
+        assertEquals(18, cityMenuItems.size());
+
+        cityMenuItems.get(7).click();
+
+        $("[data-test-id='date'] input").click();
+
+        $(".calendar")
+                .shouldBe(Condition.visible, Duration.ofSeconds(2));
+
+        Date dateNow = new Date();
+        Date futureDate = DateUtils.addWeeks(dateNow, 1);
+        futureDate = DateUtils.setHours(futureDate, 0);
+        futureDate = DateUtils.setMinutes(futureDate, 0);
+        futureDate = DateUtils.setSeconds(futureDate, 0);
+        futureDate = DateUtils.setMilliseconds(futureDate, 0);
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
+        String dateForInput = formatForDateNow.format(futureDate);
+
+        long futureDateUnixTime = futureDate.getTime();
+        String dayButtonSelector = "[data-day='" + futureDateUnixTime + "']";
+        SelenideElement dayElement = $(dayButtonSelector);
+        if (!dayElement.exists()) {
+            $("[data-step='1']").click();
+            dayElement = $(dayButtonSelector);
+        }
+
+        dayElement.click();
+
+        $("[data-test-id='name'] input").setValue("Петров Игорь");
+        $("[data-test-id='phone'] input").setValue("+79226584321");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $$("button[type='button']")
+                .findBy(Condition.text("Забронировать"))
+                .click();
+        $("fieldset").shouldHave(Condition.attribute("disabled"), Duration.ofSeconds(2));
+        $("fieldset").shouldNotHave(Condition.attribute("disabled"), Duration.ofSeconds(15));
+        $("[data-test-id='notification']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(5))
+                .shouldHave(Condition.text("Успешно!\nВстреча успешно забронирована на " + dateForInput));
     }
 }
